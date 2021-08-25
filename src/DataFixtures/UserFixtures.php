dce3552606aaaf26b0251750043a99f204b87d52
hassan <derkaouihassan@gmail.com>
2021-08-25 17:44:29 +0200

    add new fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,11 +19,26 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // Crate a new admin
-        $this->createAdmin($manager);
+        $this->createRole($manager);
 
     }
 
-    private function createAdmin(ObjectManager $manager) {
+    private function createRole(ObjectManager $manager) {
+        $user = new User();
+        $user->setEmail('user@admin.fr');
+        $user->setFirstName('UserName');
+        $user->setLastName('User');
+        $user->setPhoneNumber('0666666666');
+        $user->setRoles(['ROLE_USER']);
+        $user->setImageName('testImage');
+        $user->setOriginalName('testImage');
+        $user->setMimeType('jpg');
+        $user->setImageSize(65);
+        $user->setPassword($this->passwordHasher->hashPassword($user,'admin1234'));
+        $manager->persist($user);
+        $manager->flush();
+
+
         $admin = new User();
         $admin->setEmail('admin@admin.fr');
         $admin->setFirstName('Admin');
@@ -36,6 +51,36 @@ class UserFixtures extends Fixture
         $admin->setImageSize(65);
         $admin->setPassword($this->passwordHasher->hashPassword($admin,'admin1234'));
         $manager->persist($admin);
+        $manager->flush();
+
+
+        $super_admin = new User();
+        $super_admin->setEmail('superadmin@admin.fr');
+        $super_admin->setFirstName('Super');
+        $super_admin->setLastName('Admin');
+        $super_admin->setPhoneNumber('0666666666');
+        $super_admin->setRoles(['ROLE_SUPER_ADMIN']);
+        $super_admin->setImageName('testImage');
+        $super_admin->setOriginalName('testImage');
+        $super_admin->setMimeType('jpg');
+        $super_admin->setImageSize(65);
+        $super_admin->setPassword($this->passwordHasher->hashPassword($super_admin,'admin1234'));
+        $manager->persist($super_admin);
+        $manager->flush();
+
+
+        $premium = new User();
+        $premium->setEmail('premium@admin.fr');
+        $premium->setFirstName('Premium');
+        $premium->setLastName('User');
+        $premium->setPhoneNumber('0666666666');
+        $premium->setRoles(['ROLE_PREMIUM']);
+        $premium->setImageName('testImage');
+        $premium->setOriginalName('testImage');
+        $premium->setMimeType('jpg');
+        $premium->setImageSize(65);
+        $premium->setPassword($this->passwordHasher->hashPassword($premium,'admin1234'));
+        $manager->persist($premium);
         $manager->flush();
     }
 }
