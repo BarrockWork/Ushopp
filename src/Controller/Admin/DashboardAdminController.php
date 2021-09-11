8b5415f1fab25bd08c452ce6469b8b11c7c0d9a1
hassan <derkaouihassan@gmail.com>
2021-09-11 12:08:56 +0200

    update dashboard admin

--- src/Controller/Admin/DashboardAdminController.php
+++ src/Controller/Admin/DashboardAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CommentRepository;
 use App\Repository\OrderShopRepository;
 use App\Services\StatsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,15 +16,17 @@ class DashboardAdminController extends AbstractController
      * Dashboard of the admin
      * @Route("/{_locale}/admin", name="admin_dashboard")
      */
-    public function index(StatsService $statsService, OrderShopRepository $repo): Response
+    public function index(StatsService $statsService, OrderShopRepository $repo, CommentRepository $commentRepository): Response
     {
         $stats = $statsService->getStats();
 
         $lastOrders = $repo->getLastOrders();
+        $lastComment = $commentRepository->getLastComments();
 
         return $this->render('admin/dashboard/index_admin.html.twig', [
             'stats' => $stats,
             'lastOrders' => $lastOrders,
+            'lastComments' => $lastComment,
         ]);
     }
 }
