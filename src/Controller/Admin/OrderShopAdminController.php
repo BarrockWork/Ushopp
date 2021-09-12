91dfffda9a38d89f2f665345a22ffb0f15909025
Yannis Baltus <yannis.baltus@outlook.com>
2021-09-12 17:56:31 +0200

    [SHPP-22]Edit status of orders

--- src/Controller/Admin/OrderShopAdminController.php
+++ src/Controller/Admin/OrderShopAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Classe\Mail;
 use App\Entity\OrderShop;
 use App\Form\OrderType;
 use App\Repository\OrderShopRepository;
@@ -10,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/{_locale}/admin/order")
@@ -43,7 +45,7 @@ class OrderShopAdminController extends AbstractController
     /**
      * @Route("/{id}/edit", name="admin_order_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, OrderShop $order): Response
+    public function edit(Request $request, TranslatorInterface $translator, OrderShop $order): Response
     {
         $form = $this->createForm(OrderType::class, $order);
         $form->handleRequest($request);
@@ -51,7 +53,23 @@ class OrderShopAdminController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('order_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash(
+                'success',
+                $translator->trans('order.successEdit')
+            );
+
+            // Send an email
+            $mail = new Mail();
+            $userInfo = $order->getUser()->getFirstname();
+            $content = $translator->trans('mail.order.status',  ['%user%' => $userInfo]);
+            $mail->send(
+                $order->getUser()->getEmail(),
+                $userInfo,
+                $translator->trans('mail.order.statusSubject'),
+                $content
+            );
+
+            return $this->redirectToRoute('admin_order_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('admin/order/edit.html.twig', [
