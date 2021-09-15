da777cd64b065c482672acb68068e900c1408b00
SandRatiaray <s.ratiaray@gmail.com>
2021-09-15 12:17:52 +0200

    add phone number assert

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -100,6 +100,7 @@ class User implements UserInterface
      *     minMessage="user.lastname.minLength",
      *     maxMessage="user.lastname.maxLength"
      * )
+     * @Assert\Regex("^/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/gm")
      */
     private $lastName;
 
@@ -109,6 +110,13 @@ class User implements UserInterface
      * @ORM\Column(type="string", length=20)
      * @Assert\NotBlank(
      *     message="global.notBlank"
+     * )
+     * @Assert\Length(
+     *     min=8,
+     *     max=20,
+     *     minMessage="user.phoneNumber.minLength",
+     *     maxMessage="user.phoneNumber.maxLength"
+
      * )
      */
     private $phoneNumber;
